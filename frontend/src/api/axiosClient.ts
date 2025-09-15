379c309c888dd43d81ae6071b6797cc8406a253e
Mitesh Agrawal <miteshagrawal972@gmail.com>
2025-09-15 09:56:02 +0530

    backend url change

--- frontend/src/api/axiosClient.ts
+++ frontend/src/api/axiosClient.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const axiosClient = axios.create({
     baseURL: window.location.hostname === "localhost"
     ? "http://localhost:3000"
-    : "https://api.book4myhealth.tech",
+    : "https://api.book4myhealth.info",
     withCredentials:true,
     headers:{
         "Content-Type":"application/json"
